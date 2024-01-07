935ffbc64a26f35a44d67f1391809d5f5998f821
Kroes Soda <sodait25@gmail.com>
2024-01-07 23:39:29 +0700

    api with spring boot student crud

---
+++ src/test/java/com/java/learning/crudstudent/CrudStudentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.learning.crudstudent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudStudentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
